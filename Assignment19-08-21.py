cdddaf233688de0d44c60ea59b4fb455d168091d
Prashantdasari21 <prashantdasari143@gmail.com>
2021-08-19 13:03:22 -0700

    first commit

---
+++ Assignment19-08-21.py
@@ -0,0 +1,28 @@
+print("Answer 1")
+s="Python Skills"
+print("The string in the reverse order is ",s[::-1])
+
+print("Answer 2")
+x="19-08-2021"
+print("Year : ",x[6:10])
+
+print("Answer 3")
+x1=1100
+x2=500
+avg=1000
+add=x1+x2
+x3=avg*3-add
+print("The third employee will earn ",x3)
+
+print("Answer 4")
+from fractions import Fraction
+percent=30 
+print("The numerator/denominator form of 30% is : ",Fraction(30,100))
+
+print("Answer 5")
+train=340
+tunnel=160
+total=train+tunnel
+speed=45000/3600
+time=total/speed
+print("Time taken by the train to cross the 160m tunnel is = ",time,"s")
